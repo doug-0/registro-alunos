a62c7f1ab8d7bd06739e851ce30c9a5ea3340b58
douglas oliveira <douglasgama0@gmail.com>
2022-10-08 15:39:06 -0300

    feat: adicionando header e footer na renderização

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <v-app>
+  <v-app style="background-color: #ebebeb;">
+    <HeaderPage />
     <v-main>
       <router-view />
     </v-main>
+    <FooterPage />
   </v-app>
 </template>
 
 <script>
+import FooterPage from './components/FooterPage.vue'
+import HeaderPage from './components/HeaderPage.vue'
 
 export default {
   name: 'App',
-
+  components: {
+    FooterPage,
+    HeaderPage,
+  },
   data: () => ({
     //
   }),
