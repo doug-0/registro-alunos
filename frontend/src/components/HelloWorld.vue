b92dcdb0ca4437a0e527269e0b8bc9c930198692
douglas oliveira <douglasgama0@gmail.com>
2022-10-08 13:27:18 -0300

    reorganizando arquivos iniciais

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,34 +1,13 @@
 <template>
-  <div class="hello">
-    <h1>Olá</h1>
-  </div>
+  <v-container>
+    <h1>Olá Vuetify</h1>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+
+  data: () => ({}),
 }
 </script>
-
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
