d4d63c7ede3777c4dc8925525068c0fae34635f6
douglas oliveira <douglasgama0@gmail.com>
2022-10-12 17:42:49 -0300

    refactor: ajustando seeders api

--- backend/Model/Students.cs
+++ backend/Model/Students.cs
@@ -5,7 +5,6 @@ namespace backend.Model
     public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
-    public string RA { get; set; }
     public string CPF { get; set; }
   }
 }
