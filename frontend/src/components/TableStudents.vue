f989a839f6ad474253ebf656cc11659bf80271d5
douglas oliveira <douglasgama0@gmail.com>
2022-10-12 11:26:24 -0300

    ajustando erro lint

--- frontend/src/components/TableStudents.vue
+++ frontend/src/components/TableStudents.vue
@@ -1,4 +1,3 @@
-<!-- eslint-disable vue/valid-v-slot -->
 <template>
   <v-data-table :headers="headers" :items="students" sort-by="name" class="elevation-1">
     <template v-slot:top>
@@ -61,7 +60,7 @@
         </v-dialog>
       </v-toolbar>
     </template>
-    <template v-slot:item.actions="{ item }">
+    <template v-slot:[`item.actions`]="{ item }">
       <v-icon small class="mr-2" @click="editItem(item)">
         mdi-pencil
       </v-icon>
